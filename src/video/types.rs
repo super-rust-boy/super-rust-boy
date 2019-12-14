5159d62c7a69d2f1caac3b29ff9a0c0e2744a992
Simon Cooper <simon@movi.ai>
2019-12-14 15:54:34 +0100

    Split shaders up into different files, added missing types file

--- src/video/types.rs
+++ src/video/types.rs
@@ -1,16 +1,5 @@
-
 #[derive(Default, Copy, Clone)]
 pub struct Vertex {
     pub position: [f32; 2],
     pub data: u32
-}
-
-#[derive(Clone, Debug)]
-pub struct PushConstants {
-    pub tex_size:       [f32; 2],
-    pub atlas_size:     [f32; 2],
-    pub vertex_offset:  [f32; 2],
-    pub tex_offset:     u32,
-    pub palette_offset: u32,
-    pub flags:          u32
 }
