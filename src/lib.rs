6cf6625415178dae99ae149f2b557b35268c984c
Sean Inge Asbjørnsen <SeanAsby@users.noreply.github.com>
2019-12-18 10:49:08 +0100

    Removed C-bindings (#5)

--- src/lib.rs
+++ src/lib.rs
@@ -23,11 +23,7 @@ pub use joypad::{
     Directions
 };
 
-use std::ffi::{c_void, CStr};
-use std::os::raw::c_char;
 use std::sync::mpsc::{channel, Receiver};
-
-use winit::EventsLoop;
 
 use cpu::CPU;
 use audio::{
@@ -102,42 +98,4 @@ impl RustBoy {
     pub fn get_mem_at(&self, loc: u16) -> u8 {
         self.cpu.get_mem_at(loc)
     }
-}
-
-#[no_mangle]
-pub extern fn rustBoyCreate(cartridge_path: *const c_char, save_file_path: *const c_char) -> *const c_void {
-
-	if cartridge_path.is_null() {
-		println!("Cartridge path is null");
-		return std::ptr::null()
-	}
-
-	let cart_path_result = unsafe { CStr::from_ptr(cartridge_path) };
-	let cart_path = match cart_path_result.to_str() {
-		Ok(c) => c,
-		Err(_) => {
-			println!("Failed to parse cartridge path");
-			return std::ptr::null()
-		}
-	};
-
-	if save_file_path.is_null() {
-		println!("Save file path is null");
-		return std::ptr::null()
-	}
-
-	let save_path_result = unsafe { CStr::from_ptr(save_file_path) };
-	let save_path = match save_path_result.to_str() {
-		Ok(c) => c,
-		Err(_) => {
-			println!("Failed to parse save file path");
-			return std::ptr::null()
-		}
-	};
-
-	let events_loop = EventsLoop::new();
-	let renderer = VulkanRenderer::new(WindowType::Winit(&events_loop));
-	let instance = RustBoy::new(cart_path, save_path, UserPalette::Default, false, renderer);
-
-	Box::into_raw(instance) as *const c_void
 }
