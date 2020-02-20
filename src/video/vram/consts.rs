09011952cc5e62820c9334aa2eaea580b9f84072
simon <simon@jotta.no>
2020-02-20 18:27:21 +0100

    Removed unused consts

--- src/video/vram/consts.rs
+++ src/video/vram/consts.rs
@@ -1,7 +1,3 @@
 pub const TILE_DATA_WIDTH: usize = 16;      // Width of the tile data in tiles.
 pub const TILE_DATA_HEIGHT_GB: usize = 24;  // Height of the tile data in tiles for GB.
 pub const TILE_DATA_HEIGHT_CGB: usize = 48; // Height of the tile data in tiles for GB Color.
-pub const MAP_SIZE: usize = 32;             // Width / Height of bg/window tile maps.
-
-pub const TEX_WIDTH: usize = 8;                     // Width of a tile in pixels.
-pub const TEX_HEIGHT: usize = 8;                    // Height of a tile in pixels.
